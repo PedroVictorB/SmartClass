4f7a39518407aa55b44059ab557420b3c07de5f9
pedrovb <pedrovictor_caldas@hotmail.com>
2016-10-31 12:33:11 -0300

    Retorno de socket melhorado.Tratamento de retorno usando o Apache

--- src/smartclass/util/NioSocketServer.java
+++ src/smartclass/util/NioSocketServer.java
@@ -2,6 +2,7 @@ package smartclass.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -13,8 +14,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import smartclass.entities.Attributes;
-import smartclass.entities.ContextResponsesContainer;
 import smartclass.entities.ContextResponsesSubscriptionContainer;
 
 public class NioSocketServer {
@@ -25,82 +24,81 @@ public class NioSocketServer {
         try {
             listener = AsynchronousServerSocketChannel.open().bind(new InetSocketAddress(1026));
             // Espera um request
-        listener.accept(null, new CompletionHandler<AsynchronousSocketChannel, Void>() {
+            listener.accept(null, new CompletionHandler<AsynchronousSocketChannel, Void>() {
 
-            @Override
-            public void completed(AsynchronousSocketChannel ch, Void att) {
-                // Aceita a conexão
-                listener.accept(null, this);
-                
-                // Escreve a volta para o cliente (não necessário)
-                ch.write(ByteBuffer.wrap("Conversando!!!\n".getBytes()));
+                @Override
+                public void completed(AsynchronousSocketChannel ch, Void att) {
+                    // Aceita a conexão
+                    listener.accept(null, this);
 
-                // Buffer para ler mensagem do cliente (4K)
-                ByteBuffer byteBuffer = ByteBuffer.allocate(4096);
-                try {
-                    int bytesRead = ch.read(byteBuffer).get(20, TimeUnit.SECONDS);
+                    // Escreve a volta para o cliente (não necessário)
+                    ch.write(ByteBuffer.wrap("Conversando!!!\n".getBytes()));
 
-                    boolean running = true;
-                    while (bytesRead != -1 && running) {
-                        // Checagem de mensagem nula
-                        if (byteBuffer.position() > 2) {
-                            byteBuffer.flip();
-
-                            byte[] lineBytes = new byte[bytesRead];
-                            byteBuffer.get(lineBytes, 0, bytesRead);
-                            String line = new String(lineBytes);
-
-                            String line2 = line.substring(line.indexOf("\r\n")+225);
-                            ObjectMapper mapper = new ObjectMapper();
-                            ContextResponsesSubscriptionContainer crc = mapper.readValue(line2, ContextResponsesSubscriptionContainer.class);
-                            
-                            // Chama o serviço para mudar a UI
-                            Service s = new Service(crc.getContextResponses()[0].getContextElement().getAttributes(), crc.getContextResponses()[0].getContextElement().getId());
-
-                            ch.write(ByteBuffer.wrap(line.getBytes()));
-
-                            byteBuffer.clear();
-
-                            bytesRead = ch.read(byteBuffer).get(20, TimeUnit.SECONDS);
-                        } else {
-                            // Linha nula é o fim da mensagem
-                            running = false;
-                        }
-                    }
-                    
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                } catch (TimeoutException e) {
-                    // Timeout de 20seg
-                    ch.write(ByteBuffer.wrap("Good Bye\n".getBytes()));
-                    System.out.println("Connection timed out, closing connection");
-                } catch (IOException ex) {
-                    Logger.getLogger(NioSocketServer.class.getName()).log(Level.SEVERE, null, ex);
-                }
-
-                System.out.println("End of conversation");
-                // Fecha a conexão se necessário
-                if (ch.isOpen()) {
-                    System.out.println("Channel is open");
+                    // Buffer para ler mensagem do cliente (4K)
+                    ByteBuffer byteBuffer = ByteBuffer.allocate(4096);
                     try {
-                        ch.close();
-                        System.out.println("Channel is closed");
+                        int bytesRead = ch.read(byteBuffer).get(20, TimeUnit.SECONDS);
+
+                        boolean running = true;
+                        while (bytesRead != -1 && running) {
+                            // Checagem de mensagem nula
+                            if (byteBuffer.position() > 2) {
+                                byteBuffer.flip();
+
+                                byte[] lineBytes = new byte[bytesRead];
+                                byteBuffer.get(lineBytes, 0, bytesRead);
+                                String line = new String(lineBytes);
+                                
+                                ObjectMapper mapper = new ObjectMapper();
+                                ContextResponsesSubscriptionContainer crc = mapper.readValue(ApacheRequestFactory.getHttpMessageBodyFromString(line), ContextResponsesSubscriptionContainer.class);
+
+                                // Chama o serviço para mudar a UI
+                                Service s = new Service(crc.getContextResponses()[0].getContextElement().getAttributes(), crc.getContextResponses()[0].getContextElement().getId());
+
+                                ch.write(ByteBuffer.wrap(line.getBytes()));
+
+                                byteBuffer.clear();
+
+                                bytesRead = ch.read(byteBuffer).get(20, TimeUnit.SECONDS);
+                            } else {
+                                // Linha nula é o fim da mensagem
+                                running = false;
+                            }
+                        }
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    } catch (TimeoutException e) {
+                        // Timeout de 20seg
+                        ch.write(ByteBuffer.wrap("Good Bye\n".getBytes()));
+                        System.out.println("Connection timed out, closing connection");
                     } catch (IOException ex) {
                         Logger.getLogger(NioSocketServer.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                }
-            }
 
-            @Override
-            public void failed(Throwable exc, Void att) {
-                ///...
-            }
-        });
+                    System.out.println("End of conversation");
+                    // Fecha a conexão se necessário
+                    if (ch.isOpen()) {
+                        System.out.println("Channel is open");
+                        try {
+                            ch.close();
+                            System.out.println("Channel is closed");
+                        } catch (IOException ex) {
+                            Logger.getLogger(NioSocketServer.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                }
+
+                @Override
+                public void failed(Throwable exc, Void att) {
+                    ///...
+                }
+            });
         } catch (IOException ex) {
             Logger.getLogger(NioSocketServer.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
 }
